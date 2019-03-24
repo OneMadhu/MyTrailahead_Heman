ac0e7a7c449a8438032f2b635b44f98c68bd7c7a
Madhu B <madhu.bheemisetty-dxc@hpe.com>
2019-03-24 17:15:10 +0530

    first copy of my trailahead play ground 'HEMAN' and configured with scratch org

---
+++ .sfdx/typings/lwc/contentassets.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/contentAssetUrl/dreamhouselogosquare" {
+    var dreamhouselogosquare: string;
+    export default dreamhouselogosquare;
+}
+declare module "@salesforce/contentAssetUrl/invest_logo4" {
+    var invest_logo4: string;
+    export default invest_logo4;
+}
